29e77766f3128f936bf84a2a2e4064650077819f
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-05-16 23:24:36 +0300

    Switch to Auto version Bump

--- build/build.cmd
+++ build/build.cmd
@@ -20,8 +20,16 @@ IF NOT EXIST "%MSBUILD_PATH%" SET MSBUILD_PATH=%progpath%\Microsoft Visual Studi
 IF NOT EXIST "%MSBUILD_PATH%" SET MSBUILD_PATH=%progpath%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\bin\MSBuild.exe
 IF NOT EXIST "%MSBUILD_PATH%" SET MSBUILD_PATH=%progpath%\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\bin\MSBuild.exe
 
+:: Prepare version
+for /f "tokens=*" %%a in ('git rev-list HEAD --count') do set REVISION=%%a 
+set REVISION=%REVISION: =%
+"Tools\Tools\sed.exe" -i -r "s/(Assembly(File)?Version\(.[0-9]+\.[0-9]+\.[0-9]+\.)[0-9]+(.\))/\1%REVISION%\3/g" "MP-TVSeries\Properties\AssemblyInfo.cs"
+
 :: Build
 "%MSBUILD_PATH%" /target:Rebuild /property:Configuration=RELEASE /property:Platform=%ARCH% /fl /flp:logfile=MP-TVSeries.log;verbosity=diagnostic MP-TVSeries.sln
+
+:: Revert version
+git checkout "MP-TVSeries\Properties\AssemblyInfo.cs"
 
 CD build
 
